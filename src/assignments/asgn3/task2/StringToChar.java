3a052b86dedc80c4d0ef3086e6db31bc32bd42a1
Genie <newnewone965@gmail.com>
2024-06-15 11:12:08 +0200

    1.1

---
+++ src/assignments/asgn3/task2/StringToChar.java
@@ -0,0 +1,14 @@
+package src.assignments.asgn3.task2;
+
+public class StringToChar {
+
+    public void printChar(String input) {
+
+        for (int i = 0; i < input.length(); i++) {
+            System.out.println(input.charAt(i));
+
+        }
+    }
+}
+
+
