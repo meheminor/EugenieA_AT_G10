c1e65db691cff6718e55137bbd6701d62967b04d
Genie <newnewone965@gmail.com>
2024-06-22 11:08:28 +0200

    1.1

--- src/assignments/asgn4/GenericMethodsInGenericClassT.java
+++ src/assignments/asgn4/GenericMethodsInGenericClassT.java
@@ -1,25 +1,14 @@
 package src.assignments.asgn4;
 
-public class GenericMethodsInGenericClassT<T, S> {
-
-    T var;
-    S var2;
-
-    public T getVar() {
-        return var;
-    }
-
-    public void setVar(T var) {
-        this.var = var;
-    }
-
-    public S getVar2() {
-        return var2;
-    }
-
-    public void setVar2(S var2) {
-        this.var2 = var2;
-    }
+public class GenericMethodsInGenericClassT<T> {
+//
+//    public T getVar() {
+//        return var;
+//    }
+//
+//    public void setVar(T var) {
+//        this.var = var;
+//    }
 
     public void genericMethodOneGenArg(T var) {
         String message = String.format("I am an object of %s class", var.getClass().getSimpleName());
@@ -27,13 +16,13 @@ public class GenericMethodsInGenericClassT<T, S> {
 
     }
 
-    public String genericMethodTwoGenArgs(T var, S var2) {
+    public String genericMethodTwoGenArgs(T var, T var2) {
         return String.format("We are objects of %s class and %s class", var.getClass().getSimpleName(), var2.getClass().getSimpleName());
     }
 
-    public void genericMethodHalfGenArgs(String var, S var2){
+    public void genericMethodHalfGenArgs(String str, T var) {
 
-        String message = String.format("I got an object of %s class and string with %s characters", var2.getClass().getSimpleName(), var.length());
+        String message = String.format("I got an object of %s class and string with %s characters", var.getClass().getSimpleName(), str.length());
         System.out.println(message);
     }
 
--- src/assignments/asgn4/GenericMethodsInGenericClassTRunner.java
+++ src/assignments/asgn4/GenericMethodsInGenericClassTRunner.java
@@ -1,13 +1,51 @@
 package src.assignments.asgn4;
 
+import src.assignments.asgn3.task1.playground.essence.craft.air.Airplane;
+import src.assignments.asgn3.task1.playground.essence.craft.air.Helicopter;
+import src.assignments.asgn3.task1.playground.essence.craft.air.Rocket;
+import src.assignments.asgn3.task1.playground.essence.craft.field.Car;
+import src.assignments.asgn3.task1.playground.essence.craft.field.Motorbike;
+import src.assignments.asgn3.task1.playground.essence.craft.field.Moped;
+import src.assignments.asgn3.task1.playground.essence.creatures.Beetle;
+import src.assignments.asgn3.task1.playground.essence.creatures.Fly;
+import src.assignments.asgn3.task1.playground.essence.creatures.Raven;
+
 public class GenericMethodsInGenericClassTRunner {
     public static void main(String[] args) {
 
-        GenericMethodsInGenericClassT genericMethod1 = new GenericMethodsInGenericClassT<>();
-        GenericMethodsInGenericClassT genericMethod2 = new GenericMethodsInGenericClassT<>();
+        GenericMethodsInGenericClassT genericMethodAirplane = new GenericMethodsInGenericClassT<Airplane>();
+        GenericMethodsInGenericClassT genericMethodMotorbike = new GenericMethodsInGenericClassT<Motorbike>();
+        GenericMethodsInGenericClassT genericMethodHelicopter = new GenericMethodsInGenericClassT<Helicopter>();
+        GenericMethodsInGenericClassT genericMethodRocket = new GenericMethodsInGenericClassT<Rocket>();
+        GenericMethodsInGenericClassT genericMethodCar = new GenericMethodsInGenericClassT<Car>();
+        GenericMethodsInGenericClassT genericMethodMoped = new GenericMethodsInGenericClassT<Moped>();
+        GenericMethodsInGenericClassT genericMethodBeetle = new GenericMethodsInGenericClassT<Beetle>();
+        GenericMethodsInGenericClassT genericMethodFly = new GenericMethodsInGenericClassT<Fly>();
+        GenericMethodsInGenericClassT genericMethodRaven = new GenericMethodsInGenericClassT<Raven>();
+        Airplane airplane = new Airplane("Boeing", 3444);
+        Rocket rocket = new Rocket("Rocket", 2345);
+        Raven raven = new Raven("Raven", 18);
+        Beetle beetle = new Beetle("Beetle", 1);
+        Fly fly = new Fly(1, "Fly");
+        Moped moped = new Moped(189, "Moped");
+        Car car = new Car(190, "car");
+        Helicopter helicopter = new Helicopter("Helicopter", 1899);
+        Motorbike motorbike = new Motorbike(367, "motorbike");
+
+        genericMethodAirplane.genericMethodHalfGenArgs("Today is running" ,airplane);
+        genericMethodMoped.genericMethodHalfGenArgs("blabla",moped );
+        genericMethodHelicopter.genericMethodHalfGenArgs("no", helicopter);
+        genericMethodRocket.genericMethodHalfGenArgs("", rocket);
+        genericMethodCar.genericMethodHalfGenArgs(" . ", car);
+        genericMethodMotorbike.genericMethodHalfGenArgs("motorbike", motorbike);
+        genericMethodFly.genericMethodHalfGenArgs("I'm bored", fly);
+        genericMethodRaven.genericMethodHalfGenArgs("wtf", raven);
+        System.out.println(genericMethodBeetle.genericMethodTwoGenArgs(beetle, rocket));
 
 
-        genericMethod1.genericMethodHalfGenArgs("December is the first month of the Year.", 23);
+
+
+        //genericMethod1.genericMethodHalfGenArgs("December is the first month of the Year.", 23);
 
     }
 }
